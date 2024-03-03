94ddc1baca01e26dffffb0340d4587a44ff53d08
Dmytro Moroz <Dmytro.Moroz@ringieraxelspringer.pl>
2024-03-03 02:02:00 +0100

    further fixes

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -16,10 +16,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*res;
 
+	if (!lst)
+		return (NULL);
 	res = ft_lstnew((*f)(lst->content));
 	if (!res)
 		return (NULL);
-	res->next = NULL;
 	if (lst->next)
 	{
 		res->next = ft_lstmap(lst->next, f, del);
